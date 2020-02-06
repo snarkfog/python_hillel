aa3930cb7d503077451536c430fc4a6ffa67298b
Snarkfog <snarkfog@gmail.com>
2020-02-06 20:55:40 +0200

    Lesson 02 homework complete.

---
+++ lesson_02/escape_sequences.py
@@ -0,0 +1,20 @@
+"""
+5. Написать программу, которая выводит в консоль таблицу Escape-последовательностей:
+
+Escape sequences
+\a      Bell (alert)
+\b      Backspace
+\n      New line
+\t      Horizontal tab
+\\      Backslash \
+\"      Double quotation mark "
+\'      Single quotation mark '
+"""
+
+print("\\a      Bell (alert)")
+print("\\b      Backspace")
+print("\\n      New line")
+print("\\t      Horizontal tab")
+print("\\       Backslash \\")
+print("\\\"      Double quotation mark \"")
+print("\\'      Single quotation mark '")
---
+++ lesson_02/int_number.py
@@ -0,0 +1,23 @@
+"""
+12. Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере
+(пробелы важны!). Первая строка содержит следующее значение, а втора строка содержит предыдущее значение введёного
+числа
+
+Пример вывода:
+
+Please enter an integer number: 1234
+The next number for the number 1234 is 1235.
+The previous number for the number 1234 is 1233.
+
+или
+
+Please enter an integer number: 0
+The next number for the number 0 is 1.
+The previous number for the number 0 is -1.
+
+Вывод программы должен соответствовать примеру!
+"""
+
+int_number = int(input("Please enter an integer number: "))
+print("The next number for the number", int_number, "is", (int_number + 1))
+print("The previous number for the number", int_number, "is", (int_number - 1))
