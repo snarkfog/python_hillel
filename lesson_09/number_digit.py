c521296349141643011d2917379b2c90e6ff79c8
Snarkfog <snarkfog@gmail.com>
2020-03-28 18:35:06 +0200

    Task 10 has been updated.

--- lesson_09/number_digit.py
+++ lesson_09/number_digit.py
@@ -15,27 +15,33 @@ def num_dig(num, dig=1, way=False):
 
 
 print()
+print("Проверка без ввода:")
 print(num_dig(10))
 print(num_dig(10, 2, True))
 print()
 
-fact_num = int(input("Введите число: "))
+fact_num = int(input("Проверка со вводом.\nВведите число: "))
 default = input("По-умолчанию, выполняется сдвиг на 1 разряд влево. Желаете изменить? (y - да, n - нет): ")
+if default != "y" and default != "n":
+    print("Необходимо вводить только 'y' или 'n'")
+    exit()
 fact_dig = None
 fact_way = None
 
 if default == "y":
     fact_dig = int(input("Введите величину сдвига: "))
     fact_way = input("Введите направление сдвига (l - влево, r - вправо): ")
+    if fact_way != "l" and fact_way != "r":
+        print("Необходимо вводить только 'l' или 'r'")
+        exit()
+elif default == "n":
+    fact_dig = 1
+    fact_way = False
 
 if fact_way == "l":
     fact_way = False
 elif fact_way == "r":
     fact_way = True
 
-elif default == "n":
-    fact_dig = 1
-    fact_way = False
-
 res = num_dig(fact_num, fact_dig, fact_way)
-print("Результат сдвига: ", res)
+print("Результат сдвига:", res)
