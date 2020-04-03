6d1da683f364192e03ef9dff1e5006cde116de83
Snarkfog <snarkfog@gmail.com>
2020-04-03 20:50:27 +0300

    Task 10 has been updated.

--- lesson_09/number_digit.py
+++ lesson_09/number_digit.py
@@ -7,41 +7,30 @@
 
 
 def num_dig(num, dig=1, way=False):
-    if way:
-        num = num * (10 ** dig)
-    elif not way:
-        num = num * (10 ** -dig)
-    return num
+    num = str(num)
+    if not way:
+        res = num[len(num) - dig:len(num)] + num[:len(num) - dig]
+    else:
+        res = num[dig:len(num)] + num[:dig]
+
+    return res
 
 
-print()
-print("Проверка без ввода:")
-print(num_dig(10))
-print(num_dig(10, 2, True))
-print()
-
-fact_num = int(input("Проверка со вводом.\nВведите число: "))
-default = input("По-умолчанию, выполняется сдвиг на 1 разряд влево. Желаете изменить? (y - да, n - нет): ")
-if default != "y" and default != "n":
-    print("Необходимо вводить только 'y' или 'n'")
-    exit()
-fact_dig = None
+fact_num = int(input("Введите число: "))
+fact_dig = int(input("Введите величину сдвига: "))
 fact_way = None
 
-if default == "y":
-    fact_dig = int(input("Введите величину сдвига: "))
+while True:
     fact_way = input("Введите направление сдвига (l - влево, r - вправо): ")
-    if fact_way != "l" and fact_way != "r":
+    if fact_way == 'r':
+        fact_way = False
+        break
+    elif fact_way == 'l':
+        fact_way = True
+        break
+    else:
         print("Необходимо вводить только 'l' или 'r'")
-        exit()
-elif default == "n":
-    fact_dig = 1
-    fact_way = False
+        continue
 
-if fact_way == "l":
-    fact_way = False
-elif fact_way == "r":
-    fact_way = True
-
-res = num_dig(fact_num, fact_dig, fact_way)
-print("Результат сдвига:", res)
+fact_res = num_dig(fact_num, fact_dig, fact_way)
+print("Результат сдвига:", fact_res)
